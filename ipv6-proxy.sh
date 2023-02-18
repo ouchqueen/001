f30e403fa84aff0bf650d523c32773e99e15eb8a
ouchqueen <64894883+ouchqueen@users.noreply.github.com>
2023-02-18 16:10:50 +0800

    Update ipv6-proxy.sh

--- ipv6-proxy.sh
+++ ipv6-proxy.sh
@@ -93,10 +93,11 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create? Example 500"
-read COUNT
+#echo "How many proxy do you want to create? Example 500"
+#read COUNT
 
 FIRST_PORT=10000
+COUNT = 250
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
